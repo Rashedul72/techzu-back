3bdf29b41e6aeb803f8d6baaf1d9d55e9586b06a
Rashedul72 <muhammadrashedul8@gmail.com>
2026-05-06 15:04:00 +0600

    Update Vercel configuration with schema and framework settings; import express in API index

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
+import express from "express";
 import serverless from "serverless-http";
 
 import { createApp } from "../src/httpApp";
 
+void express;
 export default serverless(createApp(null));
